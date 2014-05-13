5341c3d265d8da81e5382877c5ec4b22f70c8d60
Dmitry Khomutov <poisoncorpsee@gmail.com>
2014-05-14 00:06:53 +0700

    Improved mysql auto installation: added auto substitution of password

--- scripts/nginx-install.sh
+++ scripts/nginx-install.sh
@@ -4,7 +4,6 @@ MODE=$1
 
 # official nginx
 cp -fv /vagrant/configs/apt/nginx.list   /etc/apt/sources.list.d/nginx.list
-
 # import key for nginx
 wget -qO - http://nginx.org/keys/nginx_signing.key | apt-key add -
 
@@ -13,7 +12,6 @@ DEBIAN_FRONTEND=noninteractive aptitude -y install nginx
 
 # nginx
 mv -fv /etc/nginx/nginx.conf /etc/nginx/nginx.origin.conf
-
 cp -fv /vagrant/configs/nginx/nginx."$MODE".conf /etc/nginx/nginx.conf
 
 mkdir /etc/nginx/sites-available
