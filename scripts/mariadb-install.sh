5a4b9a98c92a594e983eb005d024a42ab2fb123a
Corpsee <poisoncorpsee@gmail.com>
2014-05-14 00:06:53 +0700

    Improved mysql auto installation: added auto substitution of password

--- scripts/mariadb-install.sh
+++ scripts/mariadb-install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+MYSQL_ROOT_PASSWORD='root'
+
 # official mariadb repos
 cp -f /vagrant/configs/apt/mariadb.list /etc/apt/sources.list.d/mariadb.list
-
 # import key for mariadb
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
 
 DEBIAN_FRONTEND=noninteractive aptitude -y update
+
+sudo debconf-set-selections <<< "mariadb-server mariadb-server/root_password password $MYSQL_ROOT_PASSWORD"
+sudo debconf-set-selections <<< "mariadb-server mariadb-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+
 DEBIAN_FRONTEND=noninteractive aptitude -y install mariadb-server mariadb-client
 DEBIAN_FRONTEND=noninteractive aptitude -y install php5-mysql
+
+#mysql -u root -p
--- scripts/mysql-install.sh
+++ scripts/mysql-install.sh
@@ -5,6 +5,8 @@ MYSQL_ROOT_PASSWORD='root'
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
 
-DEBIAN_FRONTEND=noninteractive aptitude -y update
 DEBIAN_FRONTEND=noninteractive aptitude -y install mysql-server mysql-client
 DEBIAN_FRONTEND=noninteractive aptitude -y install php5-mysql
+
+rm -fvR /etc/php5/mods-available/20-*.ini
+[ -d /etc/apache2 ] && /etc/init.d/apache2 restart
