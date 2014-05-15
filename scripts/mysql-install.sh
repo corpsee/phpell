2e1ab08dbe140488ae7468d123adc13c4360f789
corpsee <poisoncorpsee@gmail.com>
2014-05-15 13:21:54 +0700

    Added some info

--- scripts/mysql-install.sh
+++ scripts/mysql-install.sh
@@ -11,3 +11,6 @@ DEBIAN_FRONTEND=noninteractive aptitude -y install php5-mysql
 
 rm -fvR /etc/php5/mods-available/20-*.ini
 [ -d /etc/apache2 ] && /etc/init.d/apache2 restart
+
+#mysql -u root -p
+#service mysql status
