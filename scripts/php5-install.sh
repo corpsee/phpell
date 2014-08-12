2f832a6a8875fc4bd8221ba8dc8a2e936a3f9e48
corpsee <poisoncorpsee@gmail.com>
2014-08-12 16:50:42 +0700

    Small fixes

--- scripts/php5-install.sh
+++ scripts/php5-install.sh
@@ -6,7 +6,6 @@ TIMEZONE=$2
 #TODO: php-fpm + nginx
 #TODO: move extension to script params
 #TODO: move timezone setting to script params
-#TODO: php from dotdeb or other last version
 DEBIAN_FRONTEND=noninteractive aptitude -y install php5-common php5-cli > /dev/null
 DEBIAN_FRONTEND=noninteractive aptitude -y install php5-json php5-curl php5-gd php5-imagick php5-xdebug php5-geoip php5-mcrypt php5-sqlite > /dev/null
 DEBIAN_FRONTEND=noninteractive aptitude -y install php5-dev php-pear libpcre3 libpcre3-dev > /dev/null
@@ -30,12 +29,6 @@ ln -sv /etc/php5/php.ini /etc/php5/cli/php.ini
 
 [ -d /etc/apache2 ] && ln -sv /etc/php5/mods-available /etc/php5/apache2/conf.d
 ln -sv /etc/php5/mods-available /etc/php5/cli/conf.d
-
-#TODO: ==> default: mv:
-#==> default: cannot stat ‘/etc/php5/conf.d/*’
-#==> default: : No such file or directory
-mv -fv /etc/php5/conf.d/* /etc/php5/mods-available
-rm -fvR /etc/php5/conf.d
 
 [ -d /etc/apache2 ] && cp -fv /vagrant/configs/apache2/mods/php5.conf /etc/apache2/mods-available/php5.conf
 [ -d /etc/apache2 ] && a2enmod php5
