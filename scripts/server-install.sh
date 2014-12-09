b8f2ebc4684161ab577a1b73b3876b4d3c475479
Dmitry Khomutov <poisoncorpsee@gmail.com>
2014-12-09 22:41:40 +0600

    Improved server-install script

--- scripts/server-install.sh
+++ scripts/server-install.sh
@@ -22,24 +22,26 @@ PACKAGES="mc curl htop git tar bzip2 unrar gzip unzip p7zip"
 APACHE_MODS="mpm_prefork access_compat authn_core authz_core alias deflate dir expires filter headers mime rewrite setenvif rpaf"
 
 set_locales() {
-    locale-gen "$LOCALE".utf8
-    #dpkg-reconfigure locales
+    locale-gen "en_US.utf8"
+    locale-gen "${LOCALE}.utf8"
+    locale
 }
 
 set_timezone() {
     echo "$TIMEZONE" > /etc/timezone
-    ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
-    #TODO: output to log: date
+    cp -v "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
+    date '+%F %R %:z'
 }
 
 set_packages() {
-    DEBIAN_FRONTEND=noninteractive aptitude -y update > /dev/null & aptitude -y upgrade > /dev/null
-    DEBIAN_FRONTEND=noninteractive aptitude -y install $PACKAGES > /dev/null
+    DEBIAN_FRONTEND=noninteractive aptitude -y update  > /dev/null
+    DEBIAN_FRONTEND=noninteractive aptitude -y upgrade > /dev/null
+    COMMAND="DEBIAN_FRONTEND=noninteractive aptitude -y install ${PACKAGES} > /dev/null"
+    eval "${COMMAND}"
 }
 
 set_editor() {
     update-alternatives --set editor /usr/bin/mcedit
-    #TODO: output to log: update-alternatives --query editor
 }
 
 util_install ()
@@ -55,19 +57,19 @@ util_install ()
 
 sudo -i
 
+set_packages
 set_locales
 set_timezone
-set_packages
 set_editor
 
-util_install
+#cd /home/vagrant/provision/scripts
 
-cd /home/vagrant/provision/scripts
+#./utils-install.sh
 
-./apache2-install.sh "$MODE" "$APACHE_MODS"
-./php5-install.sh    "$MODE" "$TIMEZONE"
-./nginx-install.sh   "$MODE"
-./java-install.sh    "$JAVA_VERSION"
-./user-install.sh    "$WEB_ROOT" "$WEB_USER" "$WEB_GROUP" "$WEB_USER_PASSWORD"
-./mariadb-install.sh "$MYSQL_ROOT_PASSWORD"
-./postgres-install.sh
+#./apache2-install.sh "$MODE" "$APACHE_MODS"
+#./php5-install.sh    "$MODE" "$TIMEZONE"
+#./nginx-install.sh   "$MODE"
+#./java-install.sh    "$JAVA_VERSION"
+#./user-install.sh    "$WEB_ROOT" "$WEB_USER" "$WEB_GROUP" "$WEB_USER_PASSWORD"
+#./mariadb-install.sh "$MYSQL_ROOT_PASSWORD"
+#./postgres-install.sh
