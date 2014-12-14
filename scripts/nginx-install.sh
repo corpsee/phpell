3aea876198b4f7f2a29b16dd627d37ed178595c1
corpsee <poisoncorpsee@gmail.com>
2014-12-14 15:40:20 +0600

    Fixed EOL

--- scripts/nginx-install.sh
+++ scripts/nginx-install.sh
@@ -12,7 +12,7 @@ DEBIAN_FRONTEND=noninteractive aptitude -y install nginx > /dev/null
 mv -fv /etc/nginx/nginx.conf /etc/nginx/nginx.origin.conf
 cp -fv /vagrant/configs/nginx/nginx."$MODE".conf /etc/nginx/nginx.conf
 
-#mkdir -p /etc/nginx/sites-available
+mkdir -p /etc/nginx/sites-available
 #mkdir -p /etc/nginx/sites-enabled
 
 mv -fv /etc/nginx/conf.d/* /etc/nginx/sites-available
