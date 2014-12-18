9e17c90aa67e3db3fade050f652c39b566fe1b73
Dmitry Khomutov <poisoncorpsee@gmail.com>
2014-12-18 15:05:51 +0600

    Fixed postgres install

--- scripts/postgres-install.sh
+++ scripts/postgres-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp -fv /vagrant/configs/apt/postgres.list   /etc/apt/sources.list.d/postgres.list
-wget -qO - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+#cp -fv /vagrant/configs/apt/postgres.list   /etc/apt/sources.list.d/postgres.list
+#wget -qO - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
 DEBIAN_FRONTEND=noninteractive aptitude -y update > /dev/null
 DEBIAN_FRONTEND=noninteractive aptitude -y install postgresql > /dev/null
@@ -9,7 +9,7 @@ DEBIAN_FRONTEND=noninteractive aptitude -y install postgresql > /dev/null
 mv -fv /etc/postgresql/9.3/main/postgresql.conf /etc/postgresql/9.3/main/postgresql.origin.conf
 cp -fv /vagrant/configs/postgres/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
 
-ln -sv /var/lib/postgresql/9.3 /var/lib/postgresql/9.1
+#ln -sv /var/lib/postgresql/9.3 /var/lib/postgresql/9.1
 
 service postgresql restart
 
