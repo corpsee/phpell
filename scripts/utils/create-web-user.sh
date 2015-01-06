f5ed52912772e195458c28523dc09ea967de4321
corpsee <poisoncorpsee@gmail.com>
2015-01-07 01:15:59 +0600

    Improved helpers

--- scripts/utils/create-web-user.sh
+++ scripts/utils/create-web-user.sh
@@ -3,5 +3,5 @@
 WEB_USER=$1
 WEB_USER_PASSWORD=$2
 
-useradd -g -G www-data -d /home/"$WEB_USER" -m -s /bin/bash "$WEB_USER"
+useradd -g www-data -d /home/"$WEB_USER" -m -s /bin/bash "$WEB_USER"
 echo "$WEB_USER:$WEB_USER_PASSWORD" | chpasswd
