10f5ebd2ca34430c784e88cf49a2a3e1cb5bdae9
corpsee <poisoncorpsee@gmail.com>
2015-01-06 17:54:39 +0600

    Fixed EOL

--- scripts/utils-install.sh
+++ scripts/utils-install.sh
@@ -3,12 +3,19 @@
 SERVER_TYPE=$1
 
 cp -v /home/vagrant/provision/scripts/utils/"$SERVER_TYPE"/create-host.sh  /usr/bin/create-host
-cp -v /home/vagrant/provision/scripts/utils/"$SERVER_TYPE"/disable-host.sh /usr/bin/disable-host
-cp -v /home/vagrant/provision/scripts/utils/"$SERVER_TYPE"/enable-host.sh  /usr/bin/enable-host
-
 chmod 755 /usr/bin/create-host
+
+cp -v /home/vagrant/provision/scripts/utils/"$SERVER_TYPE"/disable-host.sh /usr/bin/disable-host
 chmod 755 /usr/bin/disable-host
+
+cp -v /home/vagrant/provision/scripts/utils/"$SERVER_TYPE"/enable-host.sh  /usr/bin/enable-host
 chmod 755 /usr/bin/enable-host
 
 cp -v /home/vagrant/provision/scripts/utils/extract.sh /usr/bin/extract
 chmod 755 /usr/bin/extract
+
+cp -v /home/vagrant/provision/scripts/utils/mysql-backup.sh /usr/bin/mysql-backup
+chmod 755 /usr/bin/mysql-backup
+
+cp -v /home/vagrant/provision/scripts/utils/postgres-backup.sh /usr/bin/postgres-backup
+chmod 755 /usr/bin/postgres-backup
