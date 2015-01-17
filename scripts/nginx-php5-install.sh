054e9c14a53676e97c3b12f2d3eb79f734336dc5
corpsee <poisoncorpsee@gmail.com>
2015-01-18 00:30:53 +0600

    Fixes for sync with master

--- scripts/nginx-php5-install.sh
+++ scripts/nginx-php5-install.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-MODE=$1
-NGINX_VERSION=$2
-TIMEZONE=$3
-PHP_EXTENSIONS=$4
-PHP_VERSION=$5
+SCRIPT_DIR=$1
+MODE=$2
+NGINX_VERSION=$3
+TIMEZONE=$4
+PHP_EXTENSIONS=$5
+PHP_VERSION=$6
 
-cd /home/vagrant/provision/scripts
+cd "${SCRIPT_DIR}/scripts"
 
-./php5-install.sh   "$MODE" "$TIMEZONE" "$PHP_EXTENSIONS" "$PHP_VERSION"
+./php5-install.sh "${SCRIPT_DIR}" "${MODE}" "${TIMEZONE}" "${PHP_EXTENSIONS}" "${PHP_VERSION}"
 ./composer-install.sh
 
 DEBIAN_FRONTEND=noninteractive aptitude -y install php5-fpm > /dev/null
 
 mv -fv /etc/php5/fpm/php.ini      /etc/php5/fpm/php.origin.ini
 mv -fv /etc/php5/fpm/php-fpm.conf /etc/php5/fpm/php-fpm.origin.conf
-sed -e "s:\${TIMEZONE}:${TIMEZONE}:g" /vagrant/configs/php5/php."$MODE".ini > /etc/php5/apache2/php.ini
+sed -e "s:\${TIMEZONE}:${TIMEZONE}:g" "${SCRIPT_DIR}/configs/php5/php.${MODE}.ini" > /etc/php5/apache2/php.ini
 
 [ -f /etc/php5/mods-available/mcrypt.ini ] && php5enmod mcrypt
 
 service php5-fpm restart
 
-./nginx-install.sh  "$MODE" "$NGINX_VERSION"
+./nginx-install.sh "${SCRIPT_DIR}" "${MODE}" "${NGINX_VERSION}"
