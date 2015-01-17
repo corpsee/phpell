6e6beb5058e30b01d4f7d07fc99f9ad3e96a3c35
corpsee <poisoncorpsee@gmail.com>
2015-01-17 23:45:35 +0600

    Changed absolute paths to variable

--- scripts/mariadb-install.sh
+++ scripts/mariadb-install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-MYSQL_ROOT_PASSWORD=$1
-MARIADB_VERSION=$2
+SCRIPT_DIR=$1
+MYSQL_ROOT_PASSWORD=$2
+MARIADB_VERSION=$3
 
-if [ "$MARIADB_VERSION" == "5.5" ]; then
+if [ "${MARIADB_VERSION}" == "5.5" ]; then
     DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/mariadb-5.5
 else
     DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/mariadb-10.0
@@ -11,14 +12,14 @@ fi
 
 DEBIAN_FRONTEND=noninteractive aptitude -y update > /dev/null
 
-sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
-sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password password ${MYSQL_ROOT_PASSWORD}"
+sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password ${MYSQL_ROOT_PASSWORD}"
 
 DEBIAN_FRONTEND=noninteractive aptitude -y install mariadb-server mariadb-client > /dev/null
 
 #TODO: variable for memory setting
 mv -fv /etc/mysql/my.cnf /etc/mysql/my.origin.cnf
-cp -fv /vagrant/configs/mariadb/my.cnf /etc/mysql/my.cnf
+cp -fv "${SCRIPT_DIR}/configs/mariadb/my.cnf" /etc/mysql/my.cnf
 
 rm /var/lib/mysql/ib_logfile*
 
--- scripts/php5-install.sh
+++ scripts/php5-install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-MODE=$1
-TIMEZONE=$2
-PHP_EXTENSIONS=$3
-PHP_VERSION=$4
+SCRIPT_DIR=$1
+MODE=$2
+TIMEZONE=$3
+PHP_EXTENSIONS=$4
+PHP_VERSION=$5
 
-if [ "$PHP_VERSION" == "5.4" ]; then
+if [ "${PHP_VERSION}" == "5.4" ]; then
     DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/php5-oldstable
-elif [ "$PHP_VERSION" == "5.5" ]; then
+elif [ "${PHP_VERSION}" == "5.5" ]; then
     DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/php5
 else
     DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/php5-5.6
@@ -26,6 +27,6 @@ eval "${COMMAND}"
 #pecl install SPL_Types
 
 mv -fv /etc/php5/cli/php.ini /etc/php5/cli/php.origin.ini
-sed -e "s:\${TIMEZONE}:${TIMEZONE}:g" /vagrant/configs/php5/php."$MODE".ini > /etc/php5/cli/php.ini
+sed -e "s:\${TIMEZONE}:${TIMEZONE}:g" "${SCRIPT_DIR}/configs/php5/php.${MODE}.ini" > /etc/php5/cli/php.ini
 
 [ -f /etc/php5/mods-available/mcrypt.ini ] && php5enmod mcrypt
