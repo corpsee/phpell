aa25ed2c4b7b03229b1ef6178519e5c0e2d15bfd
corpsee <poisoncorpsee@gmail.com>
2015-01-18 02:48:13 +0600

    Fixes

--- scripts/server-install.sh
+++ scripts/server-install.sh
@@ -6,26 +6,26 @@ source "${SCRIPT_DIR}/config.sh"
 cd "${SCRIPT_DIR}/scripts"
 sudo chmod -R +x ../config.sh && sudo chmod -R +x ./
 
-sudo server-init.sh "${SCRIPT_DIR}" "${LOCALE}" "${TIMEZONE}" "${PACKAGES}" "${EDITOR}" "${VIEW}"
+sudo ./server-init.sh "${SCRIPT_DIR}" "${LOCALE}" "${TIMEZONE}" "${PACKAGES}" "${EDITOR}" "${VIEW}"
 
 if [ "${INSTALL_JAVA}" = true ]; then
-    sudo java-install.sh "${JAVA_VERSION}"
+    sudo ./java-install.sh "${JAVA_VERSION}"
 fi
 
 if [ "${INSTALL_NGINX_APACHE2}" = true ]; then
-    sudo nginx-apache2-php5-install.sh "${SCRIPT_DIR}" "${MODE}" "${APACHE_MODS}" "${TIMEZONE}" "${PHP_EXTENSIONS}" "${PHP_VERSION}" "${NGINX_VERSION}"
-    sudo utils-install.sh "${SCRIPT_DIR}" nginx_apache2
+    sudo ./nginx-apache2-php5-install.sh "${SCRIPT_DIR}" "${MODE}" "${APACHE_MODS}" "${TIMEZONE}" "${PHP_EXTENSIONS}" "${PHP_VERSION}" "${NGINX_VERSION}"
+    sudo ./utils-install.sh "${SCRIPT_DIR}" nginx_apache2
 elif [ "${INSTALL_APACHE2}" = true ]; then
-    sudo apache2-php5-install.sh "${SCRIPT_DIR}" "${MODE}" "${APACHE_MODS}" "${TIMEZONE}" "${PHP_EXTENSIONS}" "${PHP_VERSION}"
-    sudo utils-install.sh "${SCRIPT_DIR}" apache2
+    sudo ./apache2-php5-install.sh "${SCRIPT_DIR}" "${MODE}" "${APACHE_MODS}" "${TIMEZONE}" "${PHP_EXTENSIONS}" "${PHP_VERSION}"
+    sudo ./utils-install.sh "${SCRIPT_DIR}" apache2
 fi
 
 if [ "${INSTALL_MARIADB}" = true ]; then
-    sudo mariadb-php5-install.sh "${SCRIPT_DIR}" "${MYSQL_ROOT_PASSWORD}" "${MARIADB_VERSION}"
+    sudo ./mariadb-php5-install.sh "${SCRIPT_DIR}" "${MYSQL_ROOT_PASSWORD}" "${MARIADB_VERSION}"
 elif [ "${INSTALL_MYSQL}" = true ]; then
-    sudo mysql-php5-install.sh "${SCRIPT_DIR}" "${MYSQL_ROOT_PASSWORD}" "${MYSQL_VERSION}"
+    sudo ./mysql-php5-install.sh "${SCRIPT_DIR}" "${MYSQL_ROOT_PASSWORD}" "${MYSQL_VERSION}"
 fi
 
 if [ "${INSTALL_POSTGRES}" = true ]; then
-    sudo postgres-php5-install.sh "${SCRIPT_DIR}" "${POSTGRESQL_VERSION}"
+    sudo ./postgres-php5-install.sh "${SCRIPT_DIR}" "${POSTGRESQL_VERSION}"
 fi
