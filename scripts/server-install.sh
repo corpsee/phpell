a56aab730d547850cfd1b4fb1ff3108292b028d2
corpsee <poisoncorpsee@gmail.com>
2015-01-18 02:16:13 +0600

    Added server-init.sh script

--- scripts/server-install.sh
+++ scripts/server-install.sh
@@ -3,69 +3,16 @@
 SCRIPT_DIR=$1
 source "${SCRIPT_DIR}/config.sh"
 
-set_locales() {
-    locale-gen "en_US.utf8"
-    locale-gen "${LOCALE}.utf8"
-    locale
-}
-
-set_timezone() {
-    echo "${TIMEZONE}" > /etc/timezone
-    cp -v "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
-    date '+%F %R %:z'
-}
-
-set_packages() {
-    DEBIAN_FRONTEND=noninteractive aptitude -y update  > /dev/null
-    DEBIAN_FRONTEND=noninteractive aptitude -y upgrade > /dev/null
-    COMMAND="DEBIAN_FRONTEND=noninteractive aptitude -y install ${PACKAGES} > /dev/null"
-    eval "${COMMAND}"
-}
-
-set_editor() {
-    [ -f "${EDITOR}" ] && update-alternatives --set editor "${EDITOR}"
-    [ -f "${VIEW}" ] &&   update-alternatives --set view   "${VIEW}"
-    update-alternatives --get-selections
-}
-
-set_skel() {
-    cd "${SCRIPT_DIR}/configs/skel-root"
-
-    cp -vf ./.profile     /root
-    cp -vf ./.bashrc      /root
-    cp -vf ./.bash_logout /root
-
-    cd "${SCRIPT_DIR}/configs/skel"
-
-    cp -vf ./.profile     /home/vagrant
-    cp -vf ./.bashrc      /home/vagrant
-    cp -vf ./.bash_logout /home/vagrant
-
-    chown -R vagrant:vagrant /home/vagrant/*
-    chmod -R u=rwX,go=rX     /home/vagrant/*
-
-    cp -vf ./.profile     /etc/skel
-    cp -vf ./.bashrc      /etc/skel
-    cp -vf ./.bash_logout /etc/skel
-
-    chown -R root:root   /etc/skel
-    chmod -R u=rwX,go=rX /etc/skel
-}
-
-sudo set_packages
-sudo set_locales
-sudo set_timezone
-sudo set_editor
-sudo set_skel
-
 cd "${SCRIPT_DIR}/scripts"
 
+sudo ./server-init.sh "${SCRIPT_DIR}" "${LOCALE}" "${TIMEZONE}" "${PACKAGES}" "${EDITOR}" "${VIEW}"
+
 if [ "${INSTALL_JAVA}" = true ]; then
-    sudo ./java-install.sh "$JAVA_VERSION"
+    sudo ./java-install.sh "${JAVA_VERSION}"
 fi
 
 if [ "${INSTALL_NGINX_APACHE2}" = true ]; then
-    sudo ./nginx-apache2-php5-install.sh "${SCRIPT_DIR}" "${MODE}" "${APACHE_MODS}" "{$TIMEZONE}" "${PHP_EXTENSIONS}" "${PHP_VERSION}" "${NGINX_VERSION}"
+    sudo ./nginx-apache2-php5-install.sh "${SCRIPT_DIR}" "${MODE}" "${APACHE_MODS}" "${TIMEZONE}" "${PHP_EXTENSIONS}" "${PHP_VERSION}" "${NGINX_VERSION}"
     sudo ./utils-install.sh "${SCRIPT_DIR}" nginx_apache2
 elif [ "${INSTALL_APACHE2}" = true ]; then
     sudo ./apache2-php5-install.sh "${SCRIPT_DIR}" "${MODE}" "${APACHE_MODS}" "${TIMEZONE}" "${PHP_EXTENSIONS}" "${PHP_VERSION}"
