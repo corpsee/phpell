d4d055603b556a4ad33459c5794103e8e8611ad7
corpsee <poisoncorpsee@gmail.com>
2015-01-18 23:32:31 +0600

    Fixes

--- manual.sh
+++ manual.sh
@@ -5,14 +5,15 @@ set -e
 SCRIPT_DIR=$1 #/home/vagrant
 
 sudo aptitude -y update
-sudo aptitude -y upgrade
-sudo aptitude -y install mc unzip
+sudo aptitude -y install unzip
 
 cd "${SCRIPT_DIR}"
 
-wget -O phpell.zip https://github.com/corpsee/phpell/archive/master.zip && unzip ./phpell.zip -d ./phpell_temp
+wget -O phpell.zip https://github.com/corpsee/phpell/archive/master.zip
+unzip ./phpell.zip -d ./phpell_temp
 
 mv -fv  ./phpell_temp/phpell-master ./phpell
-rm -fv  ./phpell.zip && rm -frv ./phpell_temp
+rm -fv  ./phpell.zip
+rm -frv ./phpell_temp
 
 sudo ./phpell/scripts/server-install.sh "${SCRIPT_DIR}/phpell"
