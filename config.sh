38ec97650df69fd25570159436ce5bf06797110e
corpsee <poisoncorpsee@gmail.com>
2015-02-01 15:57:50 +0600

    Improved config.sh vars order

--- config.sh
+++ config.sh
@@ -4,33 +4,34 @@ MODE="debug"
 TIMEZONE="Asia/Novosibirsk"
 LOCALE='ru_RU'
 
-INSTALL_JAVA=false
-INSTALL_NGINX_APACHE2=true
-INSTALL_APACHE2=false
-INSTALL_MARIADB=true
-INSTALL_MYSQL=false
-INSTALL_POSTGRES=true
-INSTALL_MEMCACHED=true
+EDITOR="/usr/bin/mcedit"
+VIEW="/usr/bin/mcview"
+
+PACKAGES="mc curl htop git tar bzip2 unrar gzip unzip p7zip"
 
 JAVA_VERSION="8" #6|7|8
 
+PHP_VERSION="5.6" #5.6|5.5|5.4
+PHP_EXTENSIONS="php5-json php5-curl php5-gd php5-imagick php5-xdebug php5-geoip php5-mcrypt php5-sqlite"
+
+INSTALL_APACHE2=false
+APACHE_MODS="mpm_prefork access_compat authn_core authz_core alias deflate dir expires filter headers mime rewrite setenvif"
+
+INSTALL_NGINX_APACHE2=true
+NGINX_VERSION="1.7" #1.6|1.7
+
+INSTALL_JAVA=false
+
+INSTALL_MARIADB=true
+MARIADB_VERSION="10.0" #10.0|5.5
+
+INSTALL_MYSQL=false
 MYSQL_VERSION="5.6" #5.5|5.6
 MYSQL_ROOT_PASSWORD='root'
 
-MARIADB_VERSION="10.0" #10.0|5.5
-
+INSTALL_POSTGRES=true
 POSTGRESQL_VERSION="9.4" #9.3|9.4
 
-PACKAGES="mc curl htop git tar bzip2 unrar gzip unzip p7zip"
-APACHE_MODS="mpm_prefork access_compat authn_core authz_core alias deflate dir expires filter headers mime rewrite setenvif"
-
-PHP_EXTENSIONS="php5-json php5-curl php5-gd php5-imagick php5-xdebug php5-geoip php5-mcrypt php5-sqlite"
-PHP_VERSION="5.6" #5.6|5.5|5.4
-
-NGINX_VERSION="1.7" #1.6|1.7
-
-MEMCACHED_MEMCACHE=true
+INSTALL_MEMCACHED=true
+MEMCACHED_MEMCACHE=false
 MEMCACHED_MEMCACHED=true
-
-EDITOR="/usr/bin/mcedit"
-VIEW="/usr/bin/mcview"
