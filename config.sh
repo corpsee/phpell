dea497ff0c2f87ab52ee980e646cf9833793ee1d
corpsee <poisoncorpsee@gmail.com>
2015-02-22 15:11:38 +0600

    Config.sh fixes

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 MODE="debug"
+
+INIT_SERVER=true
+
 TIMEZONE="Asia/Novosibirsk"
 LOCALE='ru_RU'
 
@@ -9,29 +12,28 @@ VIEW="/usr/bin/mcview"
 
 PACKAGES="mc curl htop git tar bzip2 unrar gzip unzip p7zip"
 
+INSTALL_JAVA=false
 JAVA_VERSION="8" #6|7|8
 
 PHP_VERSION="5.6" #5.6|5.5|5.4
 PHP_EXTENSIONS="php5-json php5-curl php5-gd php5-imagick php5-xdebug php5-geoip php5-mcrypt php5-sqlite"
 
-INSTALL_APACHE2=false
+INSTALL_APACHE2=true
 APACHE_MODS="mpm_prefork access_compat authn_core authz_core alias deflate dir expires filter headers mime rewrite setenvif"
 
-INSTALL_NGINX_APACHE2=true
+INSTALL_NGINX_APACHE2=false
 NGINX_VERSION="1.7" #1.6|1.7
 
-INSTALL_JAVA=false
-
-INSTALL_MARIADB=true
+INSTALL_MARIADB=false
 MARIADB_VERSION="10.0" #10.0|5.5
 
 INSTALL_MYSQL=false
 MYSQL_VERSION="5.6" #5.5|5.6
 MYSQL_ROOT_PASSWORD='root'
 
-INSTALL_POSTGRES=true
+INSTALL_POSTGRES=false
 POSTGRESQL_VERSION="9.4" #9.3|9.4
 
 INSTALL_MEMCACHED=true
-MEMCACHED_MEMCACHE=false
+MEMCACHED_MEMCACHE=true
 MEMCACHED_MEMCACHED=true
