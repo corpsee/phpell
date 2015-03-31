cb65be1f48dff20dc42073e6dc33611dc182f6e7
corpsee <poisoncorpsee@gmail.com>
2015-04-01 00:04:48 +0600

    php5-fpm fixes

--- scripts/utils/nginx/enable-host.sh
+++ scripts/utils/nginx/enable-host.sh
@@ -4,9 +4,9 @@ test $# -eq 1 || exit
 
 HOST_NAME=$1
 
-if [ ! -f /etc/nginx/conf.d/"${HOST_NAME}".conf ]; then
-    ln -sf /etc/nginx/sites-available/"${HOST_NAME}".conf /etc/nginx/conf.d/"${HOST_NAME}".conf
+if [ ! -f /etc/nginx/sites-enabled/"${HOST_NAME}".conf ]; then
+    ln -sf /etc/nginx/sites-available/"${HOST_NAME}".conf /etc/nginx/sites-enabled/"${HOST_NAME}".conf
 fi
 
-service php5-fpm reload
+service php5-fpm restart
 service nginx    reload
