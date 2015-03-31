409ba5a000d63e5d510837ec7fd4bdb41d8332c9
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-03-31 16:29:51 +0600

    Added php-fpm configs

--- scripts/nginx-php5-install.sh
+++ scripts/nginx-php5-install.sh
@@ -14,9 +14,12 @@ cd "${SCRIPT_DIR}/scripts"
 
 DEBIAN_FRONTEND=noninteractive aptitude -y install php5-fpm > /dev/null
 
-mv -fv /etc/php5/fpm/php.ini      /etc/php5/fpm/php.origin.ini
-mv -fv /etc/php5/fpm/php-fpm.conf /etc/php5/fpm/php-fpm.origin.conf
+mv -fv /etc/php5/fpm/php.ini         /etc/php5/fpm/php.origin.ini
+mv -fv /etc/php5/fpm/php-fpm.conf    /etc/php5/fpm/php-fpm.origin.conf
+mv -fv /etc/php5/fpm/pool.d/www.conf /etc/php5/fpm/pool.d/www.origin.conf
 sed -e "s:\${TIMEZONE}:${TIMEZONE}:g" "${SCRIPT_DIR}/configs/php5/php.${MODE}.ini" > /etc/php5/apache2/php.ini
+cp -fv "${SCRIPT_DIR}/configs/php/php-fpm.conf" /etc/php5/fpm/php-fpm.conf
+cp -fv "${SCRIPT_DIR}/configs/php/www.conf" /etc/php5/fpm/pool.d/www.conf
 
 [ -f /etc/php5/mods-available/mcrypt.ini ] && php5enmod mcrypt
 
