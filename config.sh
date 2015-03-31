4aa432b40d2f28b84d4779029b095b1ebd296cf6
corpsee <poisoncorpsee@gmail.com>
2015-03-31 22:33:53 +0600

    Fixed config

--- config.sh
+++ config.sh
@@ -18,10 +18,10 @@ JAVA_VERSION="8" #6|7|8
 PHP_VERSION="5.6" #5.6|5.5|5.4
 PHP_EXTENSIONS="php5-json php5-curl php5-gd php5-imagick php5-xdebug php5-geoip php5-mcrypt php5-sqlite"
 
-INSTALL_NGINX=false
+INSTALL_NGINX=true
 NGINX_VERSION="1.7" #1.6|1.7
 
-INSTALL_APACHE2=true
+INSTALL_APACHE2=false
 APACHE_MODS="mpm_prefork access_compat authn_core authz_core alias deflate dir expires filter headers mime rewrite setenvif"
 
 INSTALL_NGINX_APACHE2=false
@@ -36,6 +36,6 @@ MYSQL_ROOT_PASSWORD='root'
 INSTALL_POSTGRES=false
 POSTGRESQL_VERSION="9.4" #9.3|9.4
 
-INSTALL_MEMCACHED=true
-MEMCACHED_MEMCACHE=true
-MEMCACHED_MEMCACHED=true
+INSTALL_MEMCACHED=false
+MEMCACHED_MEMCACHE=false
+MEMCACHED_MEMCACHED=false
