3e862cf8685af660320167f6780e6ee2de36185c
corpsee <poisoncorpsee@gmail.com>
2015-04-23 00:49:23 +0600

    Fixed postgres-backup script

--- scripts/utils/postgres-backup.sh
+++ scripts/utils/postgres-backup.sh
@@ -7,7 +7,7 @@ CURRENT_DATE=`date +%Y-%m-%d`
 
 cd /var/backups/"${DB_USER}"
 
-COMMAND="pg_dump -O -F p -U ${DB_USER} -d ${DB_NAME} | gzip > ./${DB_NAME}_${CURRENT_DATE}.sql.gz"
+COMMAND="pg_dump -O --column-inserts -F p -U ${DB_USER} -d ${DB_NAME} | gzip > ./${DB_NAME}_${CURRENT_DATE}.sql.gz"
 eval "${COMMAND}"
 
 #psql -U ${DB_USER} -d ${DB_NAME} < ./path/to/dump.sql
