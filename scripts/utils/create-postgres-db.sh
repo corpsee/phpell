fbab34613e05a444b2f31f96908536ceb820081d
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-05-02 12:38:02 +0600

    Fixed docs

--- scripts/utils/create-postgres-db.sh
+++ scripts/utils/create-postgres-db.sh
@@ -7,3 +7,5 @@ DB_PASSWORD=$3
 sudo -u postgres psql -c "CREATE USER \"${DB_USER}\" WITH PASSWORD '${DB_PASSWORD}';"
 sudo -u postgres psql -c "CREATE DATABASE \"${DB_NAME}\";"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE \"${DB_NAME}\" TO \"${DB_USER}\";"
+
+# sudo -u ${DB_NAME} psql -d ${DB_USER}
