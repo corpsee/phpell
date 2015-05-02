b49a7921760269bc7dcc4a2d4decad4ccc30b2d1
corpsee <poisoncorpsee@gmail.com>
2015-05-02 13:19:55 +0600

    Added weekly composer update

--- scripts/composer-install.sh
+++ scripts/composer-install.sh
@@ -3,3 +3,5 @@
 cd /usr/bin/
 php -r "readfile('https://getcomposer.org/installer');" | php --
 ln -sv /usr/bin/composer.phar /usr/bin/composer
+
+crontab -l | { crontab -l; echo "0 0 * * 0 composer selfupdate"; } | crontab -
