9c6616c0dce72c3121309aac13a17f8695a7ff4c
corpsee <poisoncorpsee@gmail.com>
2015-07-14 23:11:00 +0600

    Fixed README.md

--- scripts/utils-db-install.sh
+++ scripts/utils-db-install.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR=$1
 DB_TYPE=$2
 
-cp -v "${SCRIPT_DIR}/scripts/utils/${DB_TYPE}-backup.sh" "/usr/bin/${DB_TYPE}-backup"
-chmod 755 /usr/bin/${DB_TYPE}-backup
+cp -v "${SCRIPT_DIR}/scripts/utils/backup-${DB_TYPE}-db.sh" "/usr/bin/backup-${DB_TYPE}-db"
+chmod 755 /usr/bin/backup-${DB_TYPE}-db
 
 cp -v "${SCRIPT_DIR}/scripts/utils/create-${DB_TYPE}-db.sh" "/usr/bin/create-${DB_TYPE}-db"
 chmod 755 "/usr/bin/create-${DB_TYPE}-db"
