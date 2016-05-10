595ba60e51be2c6c974ba6f1b7aeb6bb1636298b
Dmitry Khomutov <poisoncorpsee@gmail.com>
2016-05-10 22:26:34 +0600

    Fixes for MySQL

--- scripts/mysql-install.sh
+++ scripts/mysql-install.sh
@@ -2,18 +2,10 @@
 
 cd "${SCRIPT_DIR}/scripts"
 
-if [ "${MYSQL_VERSION}" == "5.5" ]; then
-    DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/mysql-5.5
-else
-    DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/mysql-5.6
-fi
-
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password ${MYSQL_ROOT_PASSWORD}"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${MYSQL_ROOT_PASSWORD}"
 
 DEBIAN_FRONTEND=noninteractive aptitude -y update > /dev/null
-DEBIAN_FRONTEND=noninteractive aptitude -y install mysql-server- mysql-client > /dev/null
-
 COMMAND="DEBIAN_FRONTEND=noninteractive aptitude -y install mysql-server-${MYSQL_VERSION} mysql-client-${MYSQL_VERSION} > /dev/null"
 eval "${COMMAND}"
 
